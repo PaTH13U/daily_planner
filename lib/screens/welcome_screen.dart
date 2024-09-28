d412bb4a2b7bc6bdf7244b9101eefd86c68271b4
PaTH13U <21dh113247@st.huflit.edu.vn>
2024-09-28 12:33:52 +0700

    Refactor WelcomeScreen to dynamically load different welcome icons based on dark mode setting

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'login_screen.dart';
+import '../main.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.isDarkMode;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/images/welcomeIcon.png', width: 200),
+            Image.asset(
+              isDarkMode
+                  ? 'assets/images/welcomeIconWhite.png'
+                  : 'assets/images/welcomeIcon.png',
+              width: 200,
+            ),
             const SizedBox(height: 50),
             ElevatedButton(
               child: const Text('Đăng nhập'),
